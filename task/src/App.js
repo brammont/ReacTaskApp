b848b54943d56cd295ef2a8ed09d57de7bc33fc2
brammont <brayanmazuera1@gmail.com>
2020-08-05 11:58:56 -0500

    connected to file json to app

--- task/src/App.js
+++ task/src/App.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navigation from './components/navigation';
+import { todos } from './todos.json';
 
-function App() {
+
+class App extends Component {
+  constructor() {
+   super();
+   this.state = {
+     todos
+   }
+ }
+  render() {
+     const todos = this.state.todos.map((todo, i) => {
+       return (
+         <div className="col-md-4">
+            <div className="card mt-1">
+            <div className ="card-header">
+              <h5 class="card-title">{todo.title}</h5>
+            </div>
+            <div class="card-body">
+              <p class="card-text">{todo.description}</p>
+            </div>
+              </div>
+            </div>
+      )
+     });
+
   return (
     <div className="App">
-      <Navigation />
-      <Navigation />
-      <img src={logo} className="App-logo" alt="logo" />
+      <nav className = "navbar navbar-dark bg-dark">
+        <a href="" className="text-white">Task</a>
+      </nav>
+        <div className="container">
+          <div className="row mt-4">
+            {todos}
+          </div>
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
+}
 }
 
 export default App;
